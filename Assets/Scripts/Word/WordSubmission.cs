998d91963f1619d92559887190648a93903b2e43
Bora Çün <cun.bora@gmail.com>
2023-07-26 14:01:09 +0300

    Create binary search method

--- Assets/Scripts/Word/WordSubmission.cs
+++ Assets/Scripts/Word/WordSubmission.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class WordSubmission : MonoBehaviour
 {
+    private List<string> _validWordList;
+    
+    private void Awake()
+    {
+        _validWordList = File.ReadAllLines(@"Assets/WordList/MIT.txt").ToList();
+    }
+
     public void SubmitWord()
     {
         foreach (Transform cellTransform in transform)
@@ -11,5 +22,39 @@ public class WordSubmission : MonoBehaviour
 
             ScoreManager.Instance.IncreaseScore(cellTransform.GetChild(0).GetComponent<Letter>().LetterData.points);
         }
+    }
+
+    /**
+     * Binary Search
+     */
+    private bool IsValid(string submission)
+    {
+        int high = _validWordList.Count - 1;
+        int low = 0;
+        int mid = high / 2;
+
+        while (low <= high)
+        {
+            string midWord = _validWordList[mid];
+
+            switch (midWord.CompareTo(submission))
+            {
+                // Word found
+                case 0:
+                    return true;
+                // Check right
+                case -1:
+                    low = mid + 1;
+                    break;
+                // Check left
+                case 1:
+                    high = mid - 1;
+                    break;
+            }
+            
+            mid = (high + low) / 2;
+        }
+
+        return false;
     }
 }
