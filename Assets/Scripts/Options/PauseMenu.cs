d897c49b7b8eafcae012f82904f34c7133f1fc8e
Bora Çün <cun.bora@gmail.com>
2023-09-10 18:13:48 +0300

    Implement pause menu button functionalities

---
+++ Assets/Scripts/Options/PauseMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUIGO;
+    
+    public void OpenPauseMenu()
+    {
+        pauseMenuUIGO.SetActive(true);
+    }
+
+    public void ClosePauseMenu()
+    {
+        pauseMenuUIGO.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
